3691bff729b2e3b2bb1b43b83ca7ae679ad370fc
Guilherme Neres <guilhermeneresbsb@gmail.com>
2020-02-20 21:31:02 -0300

    Criacao do projeto de api

---
+++ src/main/java/br/com/guilherme/moneyapi/GuilhermeMoneyapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.guilherme.moneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilhermeMoneyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuilhermeMoneyapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guilherme/moneyapi/GuilhermeMoneyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guilherme.moneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilhermeMoneyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
